fab0f3eb0cde55664714bbe0c19959868ed790aa
masaless <ndkextreemo@gmail.com>
2021-04-08 14:15:35 -0300

    Primeio commit

---
+++ Estrutura de Controle/ifelseif.js
@@ -0,0 +1,23 @@
+Number.prototype.entre = function (inicio, fim) {
+    return this >= inicio && this <= fim
+}
+const imprimirResultado = function (nota) {
+    if (nota.entre(9, 10)) {
+        console.log('Quadro de Honra 👌')
+    } else if (nota.entre(7, 8.99)) {
+        console.log('Aprovado 😃')
+    } else if (nota.entre(4, 6.99)) {
+        console.log('Recuperação 🤔')
+    } else if (nota.entre(0, 3.99)) {
+        console.log('Reprovado! 😢')
+    } else {
+        console.log('Nota inválida')
+    }
+}
+
+imprimirResultado(10)
+imprimirResultado(8.99)
+imprimirResultado(6.55)
+imprimirResultado(2.3)
+imprimirResultado(-1)
+imprimirResultado(11)
